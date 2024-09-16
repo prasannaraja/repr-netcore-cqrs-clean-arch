f25823b298c95281f1558615963308d517807975
Prasanna Prabhakaran <prasannaraja@msn.com>
2024-09-16 14:08:57 +0200

    code clean up

--- src/BuildingBlocks/BuildingBlocks/CQRS/ICommandHandler.cs
+++ src/BuildingBlocks/BuildingBlocks/CQRS/ICommandHandler.cs
@@ -2,14 +2,14 @@
 
 namespace BuildingBlocks.CQRS;
 
-public interface ICommandHandler<in TCommand> 
+public interface ICommandHandler<in TCommand>
     : ICommandHandler<TCommand, Unit>
     where TCommand : ICommand<Unit>
-{ 
+{
 }
 
-public interface ICommandHandler<in TCommand, TResponse> 
-    : IRequestHandler<TCommand, TResponse> 
+public interface ICommandHandler<in TCommand, TResponse>
+    : IRequestHandler<TCommand, TResponse>
     where TCommand : ICommand<TResponse>
     where TResponse : notnull
 {
--- src/BuildingBlocks/BuildingBlocks/Pagination/PaginatedResult.cs
+++ src/BuildingBlocks/BuildingBlocks/Pagination/PaginatedResult.cs
@@ -1,6 +1,6 @@
 ﻿namespace BuildingBlocks.Pagination;
 public class PaginatedResult<TEntity>
-    (int pageIndex, int pageSize, long count, IEnumerable<TEntity> data) 
+    (int pageIndex, int pageSize, long count, IEnumerable<TEntity> data)
     where TEntity : class
 {
     public int PageIndex { get; } = pageIndex;
