b8f92741c972ccb667ab016506944f13b00444c6
Prasanna Prabhakaran <prasannaraja@msn.com>
2024-09-19 17:49:00 +0200

    MapsterConfig added

---
+++ src/Services/Weather/Weather.API/MapsterConfig.cs
@@ -0,0 +1,14 @@
+﻿using OpenMeteo;
+using Weather.API.Endpoints;
+
+namespace Weather.API
+{
+    public static class MapsterConfig
+    {
+        public static void Configure()
+        {
+            TypeAdapterConfig<WeatherForecast, GetWeatherForecastResponse>.NewConfig()
+                .Map(dest => dest.WeatherForecast, src => src);
+        }
+    }
+}
